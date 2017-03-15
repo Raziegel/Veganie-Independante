2e6ce87de9d1d92144d0342f960239b842109fd3
Maxime LB <maxime.lebail@orange.fr>
2017-03-15 13:24:45 +0100

    v

--- MobileProject/www/js/app.js
+++ MobileProject/www/js/app.js
@@ -206,14 +206,14 @@ dont on a besoin (ici en exemple $scope et $state)*/
   $scope.HotelRecu = Touriste.getHotel();
   $scope.PseudoRecu = Touriste.getPseudo();
   $scope.MotDePasseRecu = Touriste.getMotDePasse();
-var url = "https://ke-services.azurewebsites.net/tables/Utilisateur?ZUMO-API-VERSION=2.0.0";
-$http.get(url).success(function (response) {
-  alert("ok")
-         $scope.maReponseRecue = response;    }).error(function(data, status, headers, config){
-
-}).error(function(){
-  alert("erreur")
-})
+  var url = "https://ke-services.azurewebsites.net/tables/Utilisateur?ZUMO-API-VERSION=2.0.0";
+  $http.get(url).success(function (response) {
+    alert("ok")
+    $scope.maReponseRecue = response;
+  }).error(function(data, status, headers, config){
+  }).error(function(){
+    alert("erreur")
+  })
 
 })
 .controller('restaurantController', function($scope, $state){
